7351392bd30ecf68e7ed8c61f9d9c8a14d30f531
vinay <molathotivinaykumar@gmail.com>
2023-05-31 14:55:52 +0530

    addition of numbers

---
+++ AdditionOpe.java
@@ -0,0 +1,13 @@
+package project;
+
+public class AdditionOpe {
+
+	public static void main(String[] args) {
+	int num = 10;
+	int num1 = 20;
+	int result;
+	result = num+num1;
+	System.out.println("Addition of Numbers ="+result);
+	}
+
+}
