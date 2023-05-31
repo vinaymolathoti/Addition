6af1ee11180e861c7d1ccd24bca3d2a0e592d87a
vinay <molathotivinaykumar@gmail.com>
2023-05-31 15:44:09 +0530

    changes

--- AdditionOpe.java
+++ AdditionOpe.java
@@ -3,8 +3,8 @@ package project;
 public class AdditionOpe {
 
 	public static void main(String[] args) {
-	int num = 10;
-	int num1 = 20;
+	int num = 1;
+	int num1 = 2;
 	int result;
 	result = num+num1;
 	System.out.println("Addition of Numbers ="+result);
